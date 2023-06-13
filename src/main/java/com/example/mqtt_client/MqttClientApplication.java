0caacb068d12a0151b3640a93fee8b0e52f784b4
dudhdhzusyhs <136416774+dudhdhzusyhs@users.noreply.github.com>
2023-06-13 13:04:51 +0900

    Create MqttClientApplication.java

---
+++ src/main/java/com/example/mqtt_client/MqttClientApplication.java
@@ -0,0 +1,34 @@
+package com.example.mqtt_client;
+
+import io.moquette.broker.Server;
+import io.moquette.broker.config.ClasspathResourceLoader;
+import io.moquette.broker.config.IConfig;
+import io.moquette.broker.config.IResourceLoader;
+import io.moquette.broker.config.ResourceLoaderConfig;
+import io.moquette.interception.AbstractInterceptHandler;
+import io.moquette.interception.InterceptHandler;
+import io.moquette.interception.messages.InterceptPublishMessage;
+import io.netty.buffer.Unpooled;
+import io.netty.handler.codec.mqtt.MqttMessageBuilders;
+import io.netty.handler.codec.mqtt.MqttPublishMessage;
+import io.netty.handler.codec.mqtt.MqttQoS;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+
+@SpringBootApplication
+@MapperScan("com.example.mqtt_client.mapper")
+public class MqttClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttClientApplication.class, args);
+
+    }
+}
